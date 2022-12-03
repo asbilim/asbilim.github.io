cdd8c408a9fce03833f6b56fcffc4451206a382e
kali <bilimdepaullilian@gmail.com>
2022-12-04 00:52:49 +0100

    sending everything up

--- script.js
+++ script.js
@@ -20,37 +20,3 @@ connection.onmessage = (datas)=>{
         body:JSON.stringify(datas)
     })
 }
-
-$(function () {
-    var input = $('#input');
-    window.WebSocket = window.WebSocket || window.MozWebSocket;
-    if (!window.WebSocket) {
-        alert('Sorry, but your browser doesn\'t support WebSocket.')
-        window.location.href="/"
-        return;
-    }
-    var connection = new WebSocket('ws://'+window.location.hostname+'/ws');  
-    connection.onopen = function () {};  
-    connection.onerror = function (error) {
-      alert("An error occured, you will reload the page for you to access a new room !")
-      location.reload()
-    };
-    connection.onmessage = function (message) {
-      $("#chatbox").append("You: "+$("#input").val().replace('<','').replace('>','')+"\n")
-      $("#chatbox").append("\nBot: "+message["data"].replace('<','').replace('>','')+"\n-------------------------------------------------------------\n")
-      $('#chatbox').scrollTop($('#chatbox')[0].scrollHeight);
-      $("#input").val("")
-    };
-    connection.onclose = function(message) {
-      $('#chatbox').append("--------------------------END OF COMMUNICATION--------------------------")
-    }
-    input.keydown(function(e) {
-      if (e.keyCode === 13) {
-        var msg = $("#input").val();
-        if (!msg) {
-          return;
-        }
-        connection.send(msg);
-      }
-    });  
-});
